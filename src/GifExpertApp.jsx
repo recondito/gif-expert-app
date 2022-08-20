65f829ccbf423a0ee1fd41c3cb42ab6ab7127d20
Recondito <santanallanes@hotmail.com>
2022-08-20 15:13:45 -0400

    Implement button to add category.

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,15 +4,21 @@ export const GifExpertApp = () => {
 
     const [ categories, setCategories ] = useState([ 'Demon Slayer', 'One Piece' ]);
 
+    const onAddCategory = ( event ) => {
+        // setCategories( [...categories, 'Stardew Valley'] );
+        setCategories( categories => [...categories, 'Stardew Valley'] );
+    }
+
     return (
         <>
             {/* { title } */}
             <h1>GifExpertApp</h1>
             {/* {input} */}
             {/* {GIFs list} */}
+            <button onClick={ onAddCategory }>Add</button>
             <ol>
                 { categories.map(category => {
-                    return <li key={ category }>{category}</li>
+                    return <li key={ category }>{ category }</li>
                 }) }
             </ol>
                 {/* {GIF Item} */}
