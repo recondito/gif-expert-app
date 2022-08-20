a78318f12d6562530e73d5821d352f3366c53d49
Recondito <santanallanes@hotmail.com>
2022-08-20 14:46:07 -0400

    Added styles.

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,11 @@
 export const GifExpertApp = () => {
   return (
     <>
+        {/* { title } */}
         <h1>GifExpertApp</h1>
+        {/* {input} */}
+        {/* {GIFs list} */}
+            {/* {GIF Item} */}
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GifExpertApp } from './GifExpertApp';
 
+import './styles.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GifExpertApp/>
