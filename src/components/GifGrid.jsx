1068fd3ae3a8b921b23346c8c19b7fe91f96a3a9
Recondito <santanallanes@hotmail.com>
2022-09-02 17:25:05 -0400

    GIF titles appear under category.

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,15 +1,25 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
 
+    const [images, setImages] = useState([])
+
+    const getImages = async () => {
+        const newImages = await getGifs( category );
+        setImages(newImages);
+    }
+
     useEffect( () => {
-        getGifs(category);        
+        getImages();        
     }, [])
 
     return (
         <>
             <h3>{ category }</h3>
+            <ul>{
+                images.map( ({ id, title }) => (<li key={ id }>{ title }</li>))
+                }</ul>
         </>
     )
 }
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=rJVrIdmK1rWXtCFVKyWMCQl9IYjrdLtO&q=${ category }&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=rJVrIdmK1rWXtCFVKyWMCQl9IYjrdLtO&q=${ category }&limit=10`;
     const res = await fetch( url )
     const { data } = await res.json();
     const gifs = data.map( img => ({
