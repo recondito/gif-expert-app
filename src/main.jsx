167d52cfe28478a2466589c9651c8c8afca03a98
Recondito <santanallanes@hotmail.com>
2022-09-16 21:21:36 -0400

    Edited styles.css to make is more responsive. Removed Strict Mode.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { GifExpertApp } from './GifExpertApp';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GifExpertApp } from "./GifExpertApp";
 
-import './styles.css';
+import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <GifExpertApp/>
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<GifExpertApp />);
